193092ff27b6ea35139d4dab5b6cfc5493c23adf
BogdanZ19 <bogdanzainia1@gmail.com>
2025-07-23 12:19:17 +0300

    add logic and render method for conversions

--- index.js
+++ index.js
@@ -5,7 +5,35 @@
 */
 
 const inputBtn = document.getElementById("input-btn")
+const inputField = document.getElementById("get-number")
+const lengthSection = document.getElementById("length-section")
+const volumeSection = document.getElementById("volume-section")
+const massSection = document.getElementById("mass-section")
 
-// inputBtn.addEventListener("click") {
 
-// }
+inputBtn.addEventListener("click", () => {
+    let num = Number(inputField.value)
+    render(num)
+})
+
+
+function render(num) {
+    lengthSection.innerHTML += `
+        <p class="sub-text">
+            ${num} meters = ${(num * 3.281).toFixed(3)} feet | ${num} feet = 
+            ${(num / 3.281).toFixed(3)} meters
+        </p>
+    `
+    volumeSection.innerHTML += `
+        <p class="sub-text">
+            ${num} liters = ${(num / 3.785).toFixed(3)} gallons | ${num} gallons 
+            = ${(num * 3.785).toFixed(3)} liters
+        </p>
+    `
+    massSection.innerHTML += `
+        <p class="sub-text">
+            ${num} kilos = ${(num * 2.205).toFixed(3)} pounds | ${num} pounds =
+            ${(num / 2.205).toFixed(3)} kilos
+        </p>    
+    `
+}
