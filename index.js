1ba37289e1a6570c9befe542a0f2351b1b86c04e
BogdanZ19 <bogdanzainia1@gmail.com>
2025-07-23 11:48:42 +0300

    finish basic design layout

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click") {
+
+// }
